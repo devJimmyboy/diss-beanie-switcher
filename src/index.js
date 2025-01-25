b7e9ab680f83249984af3aff648e66dccc8ea32b
Jimmy Vrana <devJimmyboy@gmail.com>
2025-01-24 22:02:12 -0600

    add doom beanie

--- src/index.js
+++ src/index.js
@@ -1,50 +1,51 @@
-import { ChatClient } from "@twurple/chat";
-const url = new URL(window.location.href);
-const channel = url.searchParams.get("channel") || "dissabiIity";
+import { ChatClient } from '@twurple/chat'
+const url = new URL(window.location.href)
+const channel = url.searchParams.get('channel') || 'dissabiIity'
 const chat = new ChatClient({
   channels: [channel],
-});
-console.log("Connecting to chat...");
+})
+console.log('Connecting to chat...')
 chat.onConnect(() => {
-  console.log("Connected to chat!");
-});
-chat.connect();
+  console.log('Connected to chat!')
+})
+chat.connect()
 
 const beaniePaths = {
-  flognaw: "./videos/DissYellowPatch.mp4",
-  green: "./videos/DissYellowPatch.mp4",
-  snorlax: "./videos/DissSnorlax.mp4",
-  onepiece: "./videos/DissOnePiece.mp4",
-  mask: "./videos/DissMask.mp4",
-  web: "./videos/DissWeb.mp4",
-  spider: "./videos/DissWeb.mp4",
-};
-const videoEl = document.getElementById("video");
-videoEl.src = beaniePaths.flognaw;
+  flognaw: './videos/DissYellowPatch.mp4',
+  green: './videos/DissYellowPatch.mp4',
+  snorlax: './videos/DissSnorlax.mp4',
+  onepiece: './videos/DissOnePiece.mp4',
+  mask: './videos/DissMask.mp4',
+  web: './videos/DissWeb.mp4',
+  spider: './videos/DissWeb.mp4',
+  doom: './videos/DissDoom.mp4',
+}
+const videoEl = document.getElementById('video')
+videoEl.src = beaniePaths.flognaw
 
 chat.onMessage((channel, user, message, msg) => {
-  console.log("Message received:", message);
-  if (
-    !msg.userInfo.isMod &&
-    !msg.userInfo.isBroadcaster &&
-    user !== "devjimmyboy"
-  )
-    return;
+  console.log('Message received:', message)
+  if (!msg.userInfo.isMod && !msg.userInfo.isBroadcaster && user !== 'devjimmyboy') return
 
-  if (message.startsWith("!beanie")) {
-    const beanie = message.split(" ")[1];
+  if (message.startsWith('!beanie')) {
+    const beanie = message.split(' ')[1]
     if (!beanie) {
-      return;
+      return
     }
-    const beaniePath = beaniePaths[beanie];
+    const beaniePath = beaniePaths[beanie]
     if (!beaniePath) {
-      return;
+      return
     }
-    videoEl.src = beaniePath;
-    videoEl.play();
+    videoEl.src = beaniePath
+    videoEl.play()
   }
-});
+  if (user === 'devjimmyboy') {
+    if (message.startsWith('!refresh')) {
+      window.location.reload()
+    }
+  }
+})
 
 videoEl.oncanplaythrough = () => {
-  videoEl.play();
-};
+  videoEl.play()
+}
